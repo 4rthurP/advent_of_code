ecfbf280b97a89f8de4e85291bb8c8097a4c9d92
4rthurP <arthurperez@hotmail.fr>
2025-12-07 10:36:18 +0000

    Solved Day 7 of 2025

---
+++ src/aoc/2025/day_7/part_1.py
@@ -0,0 +1,46 @@
+from aoc.core.puzzle import AOCPuzzle
+
+
+class AOC2025Day7Part1(AOCPuzzle):
+    #verbose_output = True
+    answer = 1672
+
+    def solve(self):
+        input = self.input
+        #self.log(input)
+
+        beam_splits = 0 # Count number of splits
+        current_line = -1 # Count current line to pass odd ones where nothing happens
+
+        for stage in self.read_input():
+            current_line += 1
+            if current_line == 0:
+                beams_positions = [len(stage) // 2 - 1] # Keep track of all beams
+                self.log(f"Beam position in first line: {beams_positions} - {stage[beams_positions[0]]}")
+                continue
+
+            if current_line % 2 == 0:
+                new_beams = []
+                for beam_position in beams_positions:
+                    if stage[beam_position] == "^":
+                        # Beam is split in two
+                        new_beams.extend(
+                                [
+                                    beam_position - 1,
+                                    beam_position + 1,
+                                ]
+                            )
+                        beam_splits += 1
+                    else:
+                        # Otherwise the beam continues at his current place
+                        new_beams.append(beam_position)
+                beams_positions = list(set(new_beams))
+
+            if self.verbose_output:
+                graph = stage
+                for beam_position in beams_positions:
+                    graph = graph[:beam_position] + "|" + graph[beam_position + 1:]
+                self.log(graph)
+                self.log(f"Current number of splits: {beam_splits}")
+
+        return beam_splits
---
+++ src/aoc/2025/day_7/part_2.py
@@ -0,0 +1,46 @@
+from aoc.core.puzzle import AOCPuzzle
+
+
+class AOC2025Day7Part2(AOCPuzzle):
+    # verbose_output = True
+    answer = 231229866702355
+
+    def solve(self):
+        input = self.input
+        current_line = -1 # Count current line to pass odd ones where nothing happens
+        beams_positions = {} # Keep track of all beams
+
+        for stage in self.read_input():
+            current_line += 1
+            if current_line == 0:
+                position = len(stage) // 2 - 1
+                beams_positions = {position: 1}
+                continue
+
+            if current_line % 2 == 1:
+                continue
+
+            new_beams = {}
+            for beam_position in list(beams_positions.keys()):
+                if stage[beam_position] == "^":
+                    # Beam is split in two
+                    new_positions = [
+                        beam_position - 1,
+                        beam_position + 1,
+                    ]
+                else:
+                    # Otherwise the beam continues at his current place
+                    new_positions = [beam_position]
+
+                for position in new_positions:
+                    if position in new_beams:
+                        new_beams[position] += beams_positions[beam_position]
+                    else:
+                        new_beams[position] = beams_positions[beam_position]
+            beams_positions = new_beams
+        self.log(f"Beams positions: {beams_positions}")
+        n_beams = 0
+        for pos in list(beams_positions.keys()):
+            n_beams += beams_positions[pos]
+
+        return n_beams
