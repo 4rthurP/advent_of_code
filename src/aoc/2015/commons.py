af89c75d157ad8ee3f9281fbe1628536899b7cdb
4rthurP <arthurperez@hotmail.fr>
2025-12-23 17:51:11 +0000

    Started Year 2015

---
+++ src/aoc/2015/commons.py
@@ -0,0 +1,5 @@
+from aoc.core.puzzle import AOCPuzzle
+
+
+class AOC2025DayX(AOCPuzzle):
+    pass
---
+++ src/aoc/2015/puzzle_template.py
@@ -0,0 +1,15 @@
+from .commons import AOC2025DayX
+
+
+class AOC2025DayXPartX(AOC2025DayX):
+    verbose_output = True
+    #answer = 0
+    example_answer = 0
+
+    def solve(self):
+        input = self.input
+        answer = 0
+        
+        self.log(input)
+
+        return answer
